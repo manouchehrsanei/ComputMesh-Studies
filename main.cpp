798fc4ade3333a80365132b65345ad0ce0274dea
Manouchehr Sanei <manouchehr.sanei@gmail.com>
2017-09-12 15:38:40 -0300

    make a linear geometery

--- main.cpp
+++ main.cpp
@@ -15,39 +15,39 @@
 
 using namespace std;
 
-TPZGeoMesh *CreateOneDLGMesh(long num_el, REAL size_el);
+TPZGeoMesh *CreateOneDGMesh(long num_el, REAL size_el);
 
 
 int main() 
 
 { 
     
-    // ******************* (Create linear meshes: 1D) ***********************************************************
+    // ******************* (Create meshes: 1D) ***********************************************************
     
     REAL domain = 1.0;
     long num_el = 10;
     REAL size_el = domain/num_el;
     
-    TPZGeoMesh *gmesh_OneDL = CreateOneDLGMesh(num_el, size_el); // function to create the 1D geometric mesh
+    TPZGeoMesh *gmesh_OneD = CreateOneDGMesh(num_el, size_el); // function to create the 1D geometric mesh
     
 
 return 0; 
 }
 
-// ************************************** Create 1D linear meshes ***************************************
+// ************************************** Create 1D meshes ***************************************
 
-TPZGeoMesh *CreateOneDLGMesh(long num_el, REAL size_el)
+TPZGeoMesh *CreateOneDGMesh(long num_el, REAL size_el)
 {
-    TPZGeoMesh * gmesh_OneDL = new TPZGeoMesh; // Initilized of TPZGeoMesh class
+    TPZGeoMesh * gmesh_OneD = new TPZGeoMesh; // Initilized of TPZGeoMesh class
     
     long geometry_dim = 1; // geometry dimension
-    std::string name("geomesh OneDL"); // geometry name
+    std::string name("Geomesh OneD"); // geometry name
     
-    gmesh_OneDL->SetName(name);
-    gmesh_OneDL->SetDimension(geometry_dim);
+    gmesh_OneD->SetName(name);
+    gmesh_OneD->SetDimension(geometry_dim);
     
     long num_nodes = num_el + 1; // Number of the nodes
-    gmesh_OneDL->NodeVec().Resize(num_nodes); // Resize of the geometry mesh
+    gmesh_OneD->NodeVec().Resize(num_nodes); // Resize of the geometry mesh
     
     
     const int physical_id = 1; // Define id for material
@@ -59,8 +59,8 @@ TPZGeoMesh *CreateOneDLGMesh(long num_el, REAL size_el)
         const REAL valElem = i * size_el;
         TPZVec <REAL> coord(3,0.);
         coord[0] = valElem;
-        gmesh_OneDL->NodeVec()[i].SetCoord(coord); // Set of cordinate on the vector
-        gmesh_OneDL->NodeVec()[i].SetNodeId(i); // The id identification
+        gmesh_OneD->NodeVec()[i].SetCoord(coord); // Set of cordinate on the vector
+        gmesh_OneD->NodeVec()[i].SetNodeId(i); // The id identification
     }
     
     // Creating linear element and  zero-dimensional boundary element
@@ -78,7 +78,7 @@ TPZGeoMesh *CreateOneDLGMesh(long num_el, REAL size_el)
         Linear_topology[0] = inod_l;
         Linear_topology[1] = inod_r;
         
-        new TPZGeoElRefPattern< pzgeom::TPZGeoLinear> (element_id, Linear_topology, physical_id, *gmesh_OneDL);
+        new TPZGeoElRefPattern< pzgeom::TPZGeoLinear> (element_id, Linear_topology, physical_id, *gmesh_OneD);
         
     }
     
@@ -87,24 +87,24 @@ TPZGeoMesh *CreateOneDLGMesh(long num_el, REAL size_el)
     
     // Left boundary condition
     point_topology[0] = 0;
-    new TPZGeoElRefPattern< pzgeom::TPZGeoPoint > (element_id, point_topology, bc0, *gmesh_OneDL);
+    new TPZGeoElRefPattern< pzgeom::TPZGeoPoint > (element_id, point_topology, bc0, *gmesh_OneD);
     element_id++;
     
     
     // Right boundary condition
     point_topology[0] = num_nodes-1;
-    new TPZGeoElRefPattern< pzgeom::TPZGeoPoint > (element_id, point_topology, bc1, *gmesh_OneDL);
+    new TPZGeoElRefPattern< pzgeom::TPZGeoPoint > (element_id, point_topology, bc1, *gmesh_OneD);
     
     
-    gmesh_OneDL->BuildConnectivity(); // Construct mesh neighbor connectivity
+    gmesh_OneD->BuildConnectivity(); // Construct mesh neighbor connectivity
     
     
-    std::ofstream outgmeshOneDL("geomesh_OneDL.txt");
-    gmesh_OneDL->Print(outgmeshOneDL);
+    std::ofstream outgmeshOneD("Geomesh_OneD.txt");
+    gmesh_OneD->Print(outgmeshOneD);
     
-    std::ofstream vtkgmeshOneDL("geomesh_OneDL.vtk");
-    TPZVTKGeoMesh::PrintGMeshVTK(gmesh_OneDL, vtkgmeshOneDL);
+    std::ofstream vtkgmeshOneD("Geomesh_OneD.vtk");
+    TPZVTKGeoMesh::PrintGMeshVTK(gmesh_OneD, vtkgmeshOneD);
     
     
-    return gmesh_OneDL;
+    return gmesh_OneD;
 }
