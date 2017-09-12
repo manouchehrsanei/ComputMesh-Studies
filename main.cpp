eb88b1b132bd21ad23ad9ac2469693d57aafa666
Manouchehr Sanei <manouchehr.sanei@gmail.com>
2017-09-12 14:51:48 -0300

    Check it

--- main.cpp
+++ main.cpp
@@ -2,11 +2,18 @@
 #include <iostream>
 #include <cmath>
 #include <fstream>
+using namespace std;
 
 int main() 
 
 { 
-
+    int a, b, resultado;
+    
+    a = 5;
+    b = 10;
+    resultado = a + b;
+    cout << a << " + " << b << " = " << resultado;
+    
 
 return 0; 
 }
